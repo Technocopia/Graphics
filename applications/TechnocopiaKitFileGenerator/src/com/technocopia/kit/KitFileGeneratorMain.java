f600a51bbb69d2dc6249b4cdd549b8c4eaaea9a5
Kevin Harrington <kharrington@neuronrobotics.com>
2012-06-01 10:53:19 -0400

    Initial stub generated for generator

---
+++ applications/TechnocopiaKitFileGenerator/src/com/technocopia/kit/KitFileGeneratorMain.java
@@ -0,0 +1,13 @@
+package com.technocopia.kit;
+
+public class KitFileGeneratorMain {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
