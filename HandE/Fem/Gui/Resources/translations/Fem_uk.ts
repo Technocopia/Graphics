65131aa3ce6eaf66223172f5bbe75c3e2df7901f
Hai Pham <hpham@LLenovo.(none)>
2013-04-14 11:36:29 -0400

    backups

---
+++ HandE/Fem/Gui/Resources/translations/Fem_uk.ts
@@ -0,0 +1,56 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS>
+  <context>
+    <name>CmdFemCreateFromShape</name>
+    <message>
+      <location filename="../../Command.cpp" line="+52"/>
+      <source>Fem</source>
+      <translation>МСЕ (Метод скінченних елементів)</translation>
+    </message>
+    <message>
+      <source>Create FEM mesh</source>
+      <translation>Створити МСЕ сітку</translation>
+    </message>
+    <message>
+      <source>Create FEM mesh from shape</source>
+      <translation>Створити МСЕ сітку з форми</translation>
+    </message>
+  </context>
+  <context>
+    <name>FemGui::HypothesisWidget</name>
+    <message>
+      <location filename="../../Hypothesis.ui" line="+14"/>
+      <source>Hypothesis</source>
+      <translation>Припущення</translation>
+    </message>
+    <message>
+      <source>Quadrangle</source>
+      <translation>Чотирикутне подві́р'я</translation>
+    </message>
+    <message>
+      <source>Maximum length</source>
+      <translation>Максимальна довжина</translation>
+    </message>
+    <message>
+      <source>Local length</source>
+      <translation>Локальна довжина</translation>
+    </message>
+    <message>
+      <source>Maximum element area</source>
+      <translation>Максимальна площа елемента</translation>
+    </message>
+  </context>
+  <context>
+    <name>Workbench</name>
+    <message>
+      <location filename="../../Workbench.cpp" line="+38"/>
+      <source>FEM</source>
+      <translation>МСЕ</translation>
+    </message>
+    <message>
+      <source>&amp;FEM</source>
+      <translation>&amp;МСЕ</translation>
+    </message>
+  </context>
+</TS>
