5e27aa0bc7fdf2e83860c022adb2e42b00d074a4
Hai Pham <hpham@LLenovo.(none)>
2013-04-14 16:14:38 -0400

    added 3dprint files

--- HandE/3DPrint/install.sh
+++ HandE/3DPrint/install.sh
@@ -9,4 +9,11 @@ rm Workbench.cpp
 rm CommandDoc.cpp
 echo -e "Copying new files over..."
 cd $HOME/the-technocopia-project/HandE/3DPrint
-
+cp CommandDoc.cpp $HOME/the-technocopia-project/third-party/free-cad/src/Gui
+cp Workbench.cpp $HOME/the-technocopia-project/third-party/free-cad/src/Gui
+echo -e "Running CMake and Make...\n"
+cd $HOME/the-technocopia-project/third-party/free-cad-build
+cmake ../free-cad
+make
+cd $HOME/the-technocopia-project/HandE/3DPrint
+cp -r /NeuronRobotics $HOME/the-technocopia-project/third-party/free-cad-build/src
---
+++ HandE/3DPrint/install.sh~
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+clear
+echo -e "Creating backup of original files...\n"
+cd $HOME/the-technocopia-project/third-party/free-cad/src/Gui
+mv CommandDoc.cpp BACKCommandDoc.cpp
+mv Workbench.cpp BACKWorkbench.cpp
+rm Workbench.cpp
+rm CommandDoc.cpp
+echo -e "Copying new files over..."
+cd $HOME/the-technocopia-project/HandE/3DPrint
+
