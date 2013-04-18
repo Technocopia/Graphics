b32777719a2ecf592a0681dd2c93f6e71b41780a
Hai Pham <hpham@LTwist.(none)>
2013-04-18 10:21:17 -0400

    submodule release

--- third-party/free-cad-build/CMakeFiles/distcheckdfsg-git.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-FILE(REMOVE_RECURSE
-  "CMakeFiles/distcheckdfsg-git"
-)
-
-# Per-language clean rules from dependency scanning.
-FOREACH(lang)
-  INCLUDE(CMakeFiles/distcheckdfsg-git.dir/cmake_clean_${lang}.cmake OPTIONAL)
-ENDFOREACH(lang)
