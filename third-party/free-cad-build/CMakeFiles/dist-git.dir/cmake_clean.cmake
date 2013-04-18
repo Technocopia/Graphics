315dc0567ca1e10bb91fe88de90a8f332c8a93f7
Hai Pham <hpham@LTwist.(none)>
2013-04-18 19:34:15 -0400

    removing free-cad and free-cad-build

--- third-party/free-cad-build/CMakeFiles/dist-git.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-FILE(REMOVE_RECURSE
-  "CMakeFiles/dist-git"
-)
-
-# Per-language clean rules from dependency scanning.
-FOREACH(lang)
-  INCLUDE(CMakeFiles/dist-git.dir/cmake_clean_${lang}.cmake OPTIONAL)
-ENDFOREACH(lang)
