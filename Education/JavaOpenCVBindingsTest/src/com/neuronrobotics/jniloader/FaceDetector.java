6e704b35ce28f6b14e4009eebecca481f6a95b20
Kevin Harrington <kharrington@neuronrobotics.com>
2013-11-04 20:11:19 -0500

    Added the image scaling and the grayscale. Now working and fast.

--- Education/JavaOpenCVBindingsTest/src/com/neuronrobotics/jniloader/FaceDetector.java
+++ Education/JavaOpenCVBindingsTest/src/com/neuronrobotics/jniloader/FaceDetector.java
@@ -1,8 +1,11 @@
 package com.neuronrobotics.jniloader;
 
+import java.awt.Graphics;
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 
 import javax.imageio.ImageIO;
 
@@ -20,10 +23,11 @@ import org.opencv.objdetect.CascadeClassifier;
 
 public class FaceDetector {
 	private Mat matImage;
-	private MatOfByte mb;
 	private MatOfRect faceDetections;
 	private CascadeClassifier faceDetector ;
 	private VideoCapture vc;
+	private double scale=.2;
+	
 	public FaceDetector(int index){
 		 System.out.println("\nRunning DetectFaceDemo");
 	    vc = new VideoCapture(0);
@@ -40,7 +44,7 @@ public class FaceDetector {
 	        System.out.println("Camera OK");
 	    }
 	    matImage = new Mat();
-		mb=new MatOfByte(); 
+		
 		faceDetections = new MatOfRect();
 		// Create a face detector from the cascade file in the resources
 	    // directory.
@@ -58,9 +62,8 @@ public class FaceDetector {
 	}
 	
 	public BufferedImage getLatestImage(){
-		if(matImage.empty()){
-			updateImage();
-		}
+
+		MatOfByte mb=new MatOfByte(); 
 		Highgui.imencode(".jpg", matImage, mb);
 		BufferedImage image=null;
 		try {
@@ -74,16 +77,58 @@ public class FaceDetector {
 	
 	public Rect [] getFaces(){
 		updateImage();
-		if(matImage.empty()){
+		BufferedImage tmp= toGrayScale(getLatestImage(),scale);
+		
+		MatOfByte mb;
+
+		try {
+			
+			ByteArrayOutputStream baos = new ByteArrayOutputStream();
+			ImageIO.write(tmp, "jpg", baos);
+			baos.flush();
+			byte[] tmpByteArray = baos.toByteArray();
+			baos.close();
+			mb = new MatOfByte(tmpByteArray);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 			return null;
 		}
-		Rect [] myArray = faceDetections.toArray();
-		faceDetector.detectMultiScale(matImage, faceDetections);
+	
+		Mat matImageLocal =Highgui.imdecode(mb, 0);
+		
+		if(matImageLocal.empty()){
+			return null;
+		}
+		
+		faceDetector.detectMultiScale(matImageLocal, faceDetections);
+		Rect [] smallArray = faceDetections.toArray();
+		Rect [] myArray = new Rect [smallArray.length];
+		for(int i=0;i<smallArray.length;i++){
+			Rect r = smallArray[i];
+			myArray[i] = new Rect(	(int)	(r.x/scale),
+									(int)(r.y/scale),
+									(int)(r.width/scale), 
+									(int)(r.height/scale));
+		}
+		
 		//System.out.println(String.format("Detected %s faces", myArray.length));
 		// Draw a bounding box around each face.
 	    for (Rect rect : myArray) {
 	        Core.rectangle(matImage, new Point(rect.x, rect.y), new Point(rect.x + rect.width, rect.y + rect.height), new Scalar(0, 255, 0));
 	    }
 		return myArray;
+	}
+	
+	private BufferedImage toGrayScale(BufferedImage in, int w, int h){
+		BufferedImage bi = new BufferedImage(w,h, BufferedImage.TYPE_BYTE_GRAY);
+		Graphics g = bi.createGraphics();
+		g.drawImage(in, 0, 0,w,h, null);
+		return bi;
+	}
+	private BufferedImage toGrayScale(BufferedImage in, double scale){
+		int w=(int)(in.getWidth()*scale);
+		int h=(int) (in.getHeight()*scale);	
+		return toGrayScale(in, w, h);
 	}
 }
